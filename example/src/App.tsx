fc188b77eed1e6cd332a7bc17b96657e574dc30d
Hans <admin@itsrv.tw>
2023-04-26 21:40:16 +0800

    Update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 import Peripheral, {
   TxPower,
   AdvertiseMode,
+  Permission,
+  Property,
 } from '@fugood/react-native-multi-ble-peripheral';
 import { Buffer } from 'buffer';
+
+const hrService = Platform.select({
+  ios: '180d',
+  default: '0000180d-0000-1000-8000-00805f9b34fb',
+});
+
+const hrCharacteristic = Platform.select({
+  ios: '2a37',
+  default: '00002a37-0000-1000-8000-00805f9b34fb',
+});
 
 export default function App() {
   const [advertising, setAdvertising] = React.useState<boolean>(false);
@@ -14,29 +32,44 @@ export default function App() {
   React.useEffect(() => {
     const ble = new Peripheral();
     peripheral.current = ble;
-    ble
-      .startAdvertising(
-        {
-          '1234': Buffer.from('1234'),
-        },
-        {
-          mode: AdvertiseMode.LOW_POWER,
-          txPower: TxPower.HIGH,
-          connectable: true,
-          includeDeviceName: true,
-          includeTxPowerLevel: true,
-          manufacturerId: 0x004c,
-          manufacturerData: Buffer.from('1234'),
-        }
-      )
-      .then(() => {
-        setAdvertising(true);
+    (async () => {
+      await ble.addService(hrService, true);
+      await ble.addCharacteristic(
+        hrService,
+        hrCharacteristic,
+        Property.READ | Property.NOTIFY, // eslint-disable-line no-bitwise
+        Permission.READABLE
+      );
+      await ble.updateValue(
+        hrService,
+        hrCharacteristic,
+        Buffer.from('22', 'hex')
+      );
+      await ble.startAdvertising({
+        mode: AdvertiseMode.LOW_POWER,
+        txPower: TxPower.HIGH,
+        connectable: true,
+        includeDeviceName: true,
+        includeTxPowerLevel: true,
       });
+      setAdvertising(true);
+    })();
+  }, []);
+
+  const notify = React.useCallback(async () => {
+    await peripheral.current?.sendNotification(
+      hrService,
+      hrCharacteristic,
+      Buffer.from('11', 'hex')
+    );
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Advertising: {advertising}</Text>
+      <TouchableOpacity onPress={notify} style={styles.box}>
+        <Text>Notify</Text>
+      </TouchableOpacity>
     </View>
   );
 }
